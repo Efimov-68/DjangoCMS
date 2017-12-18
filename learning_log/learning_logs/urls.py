7f34b52028b15b22d73a86b21a62d495163a8d44
Efimov-68 <r.efimov@list.ru>
2017-12-18 18:13:39 +0300

    learning_log project ModelForm

--- learning_log/learning_logs/urls.py
+++ learning_log/learning_logs/urls.py
@@ -4,10 +4,12 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	#Домашняя страница
-	url(r'^$', views.index, name='index'),
-	#Вывод всех тем.
-	url(r'^topics/$', views.topics, name='topics'),
-	#Страница с подробной информацией по отдельной теме
-	url(r'^topics/(?P<topic_id>\d+)/$', views.topic, name='topic'),
+    #Домашняя страница
+    url(r'^$', views.index, name='index'),
+    #Вывод всех тем.
+    url(r'^topics/$', views.topics, name='topics'),
+    #Страница с подробной информацией по отдельной теме
+    url(r'^topics/(?P<topic_id>\d+)/$', views.topic, name='topic'),
+    # Страница для добавления новой темы
+    url(r'^new_topic/$', views.new_topic, name='new_topic'),
 ]
