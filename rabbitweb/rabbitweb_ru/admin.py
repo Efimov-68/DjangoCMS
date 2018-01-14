bef30cd53731a8155abaec8461c5d59b221f7dd8
Efimov-68 <r.efimov@list.ru>
2018-01-14 17:16:20 +0300

    DjangoCMS rabbitweb_ru 140118 17:16

---
+++ rabbitweb/rabbitweb_ru/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+
+# Register your models here.
+from rabbitweb_ru.models import Topic
+admin.site.register(Topic)
---
+++ rabbitweb/rabbitweb_ru/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class RabbitwebRuConfig(AppConfig):
+    name = 'rabbitweb_ru'
